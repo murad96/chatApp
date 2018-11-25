2f8b874f0c4103b0634fe22c805dda085e3e0954
Murad Al Wajed <muradwajed@gmail.com>
2018-11-25 21:03:12 +0600

    Fetched user from database done

--- chatApp/Controller/NewMessageController.swift
+++ chatApp/Controller/NewMessageController.swift
@@ -17,11 +17,23 @@ class NewMessageController: UITableViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         print("Came")
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
         fetchUser()
     }
     func fetchUser(){
         Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
             print(snapshot)
+            if let dictionary = snapshot.value as? [String: AnyObject]{
+                let user = User()
+                
+                //user.setValuesForKeys(dictionary)
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                self.users.append(user)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
             
         }, withCancel: nil)
     }
@@ -29,11 +41,23 @@ class NewMessageController: UITableViewController {
         dismiss(animated: true, completion: nil)
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        cell.textLabel!.text = "jsahfjskdfhjdsf"
+        //let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel!.text = user.name
+        cell.detailTextLabel?.text = user.email
         return cell
+    }
+}
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
